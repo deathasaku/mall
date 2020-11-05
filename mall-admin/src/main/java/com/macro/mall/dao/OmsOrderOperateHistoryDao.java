6fd8e65f019226bc3ac9127b2a29ec33772c34e6
User <User@DESKTOP-26CJV3T.mshome.net>
2020-11-05 22:55:11 +0800

    AAAAAAA

--- mall-admin/src/main/java/com/macro/mall/dao/OmsOrderOperateHistoryDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/OmsOrderOperateHistoryDao.java
@@ -1,17 +1,20 @@
 package com.macro.mall.dao;
 
-import com.macro.mall.model.OmsOrderOperateHistory;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.macro.mall.model.OmsOrderOperateHistory;
+
 /**
- * 订单操作记录自定义Dao
- * Created by macro on 2018/10/12.
+ * 订单操作记录自定义Dao Created by macro on 2018/10/12.
  */
 public interface OmsOrderOperateHistoryDao {
-    /**
-     * 批量创建
-     */
-    int insertList(@Param("list") List<OmsOrderOperateHistory> orderOperateHistoryList);
+	/**
+	 * 批量创建 隨便啦 2ㄏ
+	 */
+	// ㄆ斯ㄆ 斯
+	String a = "1";
+
+	int insertList(@Param("list") List<OmsOrderOperateHistory> orderOperateHistoryList);
 }
